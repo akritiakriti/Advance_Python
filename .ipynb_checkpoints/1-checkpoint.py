f4cbc3b6b5178517e6a3ff9c54f9194f11dc03c8
Akriee <akritigit19@gmail.com>
2025-12-17 17:13:44 +0545

    initial commit

---
+++ .ipynb_checkpoints/1-checkpoint.py
@@ -0,0 +1,44 @@
+my_age= int(input("Enter your age:"))
+print(f"You atre {my_age} yrs old.")
+
+
+# 2
+food= input("Enter your favourite food")
+print(f"Your favourite food is:{food}")
+
+#3
+var= "42"
+int_var= int(var)
+print(f"Converted {var} to {int_var}")
+
+#4
+floa= 3.14
+str_var= str(floa)
+print(f"Converted {floa} to {str_var}")
+
+
+#5
+a= "hello"
+b="world"
+print(a+b)
+
+6
+aa= "pyython"
+count=0
+for ch in aa:
+    count+=1
+    if count==3:
+        print(ch)
+
+    
+#7
+bb = input("Enter a sentence: ")
+words = bb.split()
+for i in range(5):
+    print(words[i])
+
+     
+   
+
+
+
